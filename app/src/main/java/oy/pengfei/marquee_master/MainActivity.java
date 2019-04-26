a154f335491c3e167985675204f6bb0fede86d23
ouyangpengfei <pengfei.oyang@xiaoyangedu.com>
2019-04-26 10:59:25 +0800

    1.添加点击事件 2.删除text_scroll_to_content_end属性

--- app/src/main/java/oy/pengfei/marquee_master/MainActivity.java
+++ app/src/main/java/oy/pengfei/marquee_master/MainActivity.java
@@ -2,6 +2,8 @@ package oy.pengfei.marquee_master;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import com.xaf.marqueelib.MarqueeView;
 
@@ -15,16 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MarqueeView marquee = findViewById(R.id.marqueeView);
-        MarqueeView marquee2 = findViewById(R.id.marqueeView_2);
 
+        MarqueeView marquee = findViewById(R.id.marqueeView);
         List<String> contentList = new ArrayList<>();
         List<Integer> colorList = new ArrayList<>();
-
         colorList.add(R.color.colorAccent);
         contentList.add("1.“网上95%的名人名言都是瞎掰，包括这句。”——鲁迅");
+        //开启滚动
         marquee.setDataAndScroll(contentList, colorList);
 
+        //点击事件
+        marquee.getCurrentView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Log.d("cccc","hhhh");
+            }
+
+        });
+
+        MarqueeView marquee2 = findViewById(R.id.marqueeView_2);
         List<String> contentList2 = new ArrayList<>();
         contentList2.add("1.“网上95%的名人名言都是瞎掰，包括这句。”——鲁迅");
         contentList2.add("2.“我即使是死了，钉在棺材里，也要在墓里，用这腐朽的声带喊出：我没说过这句话”——鲁迅");
@@ -32,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         colorList.add(R.color.colorPrimary);
         colorList.add(R.color.colorPrimaryDark);
         marquee2.setDataAndScroll(contentList2, colorList);
+
 
 
     }
